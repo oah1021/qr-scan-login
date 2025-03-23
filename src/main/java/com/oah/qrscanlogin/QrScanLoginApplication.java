48c999f88783216dfbc01e56cd2c279996fab69b
JUNHAO <oah1021@163.com>
2025-03-23 16:36:08 +0800

    add: 初始化项目

---
+++ src/main/java/com/oah/qrscanlogin/QrScanLoginApplication.java
@@ -0,0 +1,13 @@
+package com.oah.qrscanlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrScanLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrScanLoginApplication.class, args);
+    }
+
+}
